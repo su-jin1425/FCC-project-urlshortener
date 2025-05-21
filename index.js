21cc161f3a89094b0ee67ff019e116b052921805
Sujith S <116474043+su-jin1425@users.noreply.github.com>
2025-05-21 12:21:26 +0530

    -

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
-
-// Basic Configuration
+const urlparser = require('url');
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(`${process.cwd()}/public`));
-
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const urlDatabase = {};
+let shortUrlId = 1;
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  try {
+    const urlObject = new URL(url);
+    if (!urlObject.protocol.includes('http')) {
+      return res.json({ error: 'invalid url' });
+    }
+    dns.lookup(urlObject.hostname, (err) => {
+      if (err) {
+        return res.json({ error: 'invalid url' });
+      }
+      urlDatabase[shortUrlId] = url;
+      res.json({
+        original_url: url,
+        short_url: shortUrlId
+      });
+      shortUrlId++;
+    });
+  } catch (err) {
+    return res.json({ error: 'invalid url' });
+  }
 });
-
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const shortUrl = req.params.short_url;
+  const originalUrl = urlDatabase[shortUrl];
+  
+  if (!originalUrl) {
+    return res.json({ error: 'No short URL found for the given input' });
+  }
+  res.redirect(originalUrl);
+});
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
